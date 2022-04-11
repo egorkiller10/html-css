fc18c14a504c48109c85f780e2311ef829ee05e9
Egor <egor-kastorsky@mail.ru>
2022-04-12 02:55:53 +0300

    js1

---
+++ Homework/html-css/javascript/js/script.js
@@ -0,0 +1,17 @@
+'use strict'
+let num1 = 4, num2 = 5;
+let sum1 = (num1+num2);
+console.log(sum1);
+let num3 = 1, num4 = 1;
+let sum2 = (num3+num4);
+console.log(sum2);
+
+let sum10 = 150000;
+let result = sum10 > 100000 ? sum10-(10*sum10 /100) : sum10;
+console.log(result);
+// сделать 3 задание 
+let a=2;
+let b=5;
+
+
+console.log(a, b);
