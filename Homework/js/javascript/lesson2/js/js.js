d8139c8dd653cc55e1b3a228f628a5c337a392bf
Egor <egor-kastorsky@mail.ru>
2022-04-18 00:26:31 +0300

    js2

---
+++ Homework/js/javascript/lesson2/js/js.js
@@ -0,0 +1,32 @@
+// let sum = 10000;
+// let ticketNum = prompt("Введите номер купона");
+// switch (ticketNum){
+//     case '4653':
+//         console.log(sum-(sum/100*30))
+//         break;
+//     case '5698':
+//     case '5111':
+//         console.log(sum-(sum/100*20));
+//         break;
+//     case '6922':
+//     case '6113':
+//     case '6099':
+//         console.log(sum-(sum/100*10))
+//         break;
+// }
+
+// let count = prompt("Введите количество баллов");
+// if(100 >= count && 90 <= count){
+//     console.log("Отлично")
+// } else if (89 >= count && 60 <= count) {
+//     console.log("Хорошо")
+// }else if(59 >= count && 40 <= count) {
+//     console.log("Удовлетворительно")
+// }else console.log("Попробуйте еще раз");
+
+let plates = 4;
+let fairy = 10;
+while (fairy > 0 && plates > 0);
+
+console.log(Math.ceil(Math.random() * 9));
+
